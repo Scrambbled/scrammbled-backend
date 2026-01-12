17570e4a3160323e9dce4514df25f06d794ce9dd
daniel.wiszniowski <mrcabbagestick@gmail.com>
2026-01-12 12:39:49 +0100

    Add static file config for user icons

---
+++ src/main/kotlin/io/github/mrcabbagestick/scrambbled/config/StaticConfig.kt
@@ -0,0 +1,15 @@
+package io.github.mrcabbagestick.scrambbled.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class StaticConfig: WebMvcConfigurer {
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry
+            .addResourceHandler("/static/user_icons/**")
+            .addResourceLocations("classpath:/static/user_icons")
+    }
+}
